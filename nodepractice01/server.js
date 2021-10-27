b7182920f02497929df256fe6535ff821b919db1
Kyril Jardinico <Kyril.Jay@gmail.com>
2021-10-26 23:44:30 -0400

    Update server.js

--- nodepractice01/server.js
+++ nodepractice01/server.js
@@ -14,4 +14,4 @@ var server = http.createServer(function(req, res){
 });
 
 server.listen(5000, '127.0.0.1');
-console.log('yo dawgs, now listening to port 3000');
+console.log('you are now listening to port 5000');
