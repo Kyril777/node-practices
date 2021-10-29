3af078d5e0ba6c45ca30a89609f566e22f99c7b4
Kyril Jardinico <Kyril.Jay@gmail.com>
2021-10-28 23:55:17 -0400

    Update main.js

--- npmapp/main.js
+++ npmapp/main.js
@@ -2,6 +2,15 @@ const _ = require('lodash');
 
 const numbers = [44,100,87,55,43,421];
 
+/*
+const members = ["Bill","Joan","Frank","Eleanor","Trent","Chris"];
+*/
+
 _.each(numbers, function(number){
   console.log(number);
 });
+
+/*
+_.each(members, function(member){
+  console.log(member);
+*/
